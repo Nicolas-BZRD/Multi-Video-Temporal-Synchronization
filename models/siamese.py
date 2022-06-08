e17e5038a47c1c0c035075a4ca7046bd966bba73
NICOLAS BZRD <nicolas.bzrd@gmail.com>
2022-06-08 15:58:58 +0200

    dropout fix

--- models/siamese.py
+++ models/siamese.py
@@ -4,6 +4,9 @@ from tensorflow.keras import models, layers, backend, callbacks, metrics
 class SiameseCNN():
     def __init__(self, dropout: list, img_shape: tuple, v=1) -> None:
         self.version = v
+
+        
+
         if(v==1):
             self._v1(dropout, img_shape)
         elif(v==2):
@@ -17,22 +20,22 @@ class SiameseCNN():
         cnn = models.Sequential()
         cnn.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=img_shape))
         cnn.add(layers.MaxPooling2D((3, 3)))
-
-        cnn.add(layers.Conv2D(48, (3, 3), activation='relu'))
-        cnn.add(layers.MaxPooling2D((2, 2)))
         cnn.add(layers.Dropout(dropout[0]))
 
         cnn.add(layers.Conv2D(48, (3, 3), activation='relu'))
         cnn.add(layers.MaxPooling2D((2, 2)))
         cnn.add(layers.Dropout(dropout[1]))
 
-        cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        cnn.add(layers.Conv2D(48, (3, 3), activation='relu'))
         cnn.add(layers.MaxPooling2D((2, 2)))
         cnn.add(layers.Dropout(dropout[2]))
 
+        cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        cnn.add(layers.MaxPooling2D((2, 2)))
+        cnn.add(layers.Dropout(dropout[3]))
+
         cnn.add(layers.GlobalAveragePooling2D())
         cnn.add(layers.Dense(48, activation='relu'))
-        cnn.add(layers.Dropout(dropout[3]))
 
         # Generate the encodings (feature vectors) for the two images
         left_feats = cnn(left_input)
@@ -58,26 +61,23 @@ class SiameseCNN():
         # Convolutional Neural Network
         cnn = models.Sequential()
         cnn.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=img_shape))
-        cnn.add(layers.MaxPooling2D((2, 2)))
-
-        cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
-        cnn.add(layers.MaxPooling2D((2, 2)))
+        cnn.add(layers.MaxPooling2D((3, 3)))
         cnn.add(layers.Dropout(dropout[0]))
 
-        cnn.add(layers.Conv2D(128, (3, 3), activation='relu'))
+        cnn.add(layers.Conv2D(48, (3, 3), activation='relu'))
         cnn.add(layers.MaxPooling2D((2, 2)))
         cnn.add(layers.Dropout(dropout[1]))
 
-        cnn.add(layers.Conv2D(96, (1, 1), activation='relu'))
+        cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
         cnn.add(layers.MaxPooling2D((2, 2)))
         cnn.add(layers.Dropout(dropout[2]))
 
         cnn.add(layers.Conv2D(96, (3, 3), activation='relu'))
         cnn.add(layers.MaxPooling2D((2, 2)))
+        cnn.add(layers.Dropout(dropout[3]))
 
         cnn.add(layers.GlobalAveragePooling2D())
         cnn.add(layers.Dense(64, activation='relu'))
-        cnn.add(layers.Dropout(dropout[3]))
 
         # Generate the encodings (feature vectors) for the two images
         left_feats = cnn(left_input)
