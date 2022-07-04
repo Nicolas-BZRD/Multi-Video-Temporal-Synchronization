f344febceedbeaec7eb82f8e127b6be5a4cae014
NICOLAS BZRD <nicolas.bzrd@gmail.com>
2022-07-04 14:24:35 +0200

    model test

--- models/siamese/siamese.py
+++ models/siamese/siamese.py
@@ -9,15 +9,19 @@ class SiameseCNN():
         # Convolutional Neural Network
         cnn = models.Sequential()
         cnn.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=img_shape))
+        cnn.add(layers.BatchNormalization())
         cnn.add(layers.MaxPooling2D((3, 3)))
 
         cnn.add(layers.Conv2D(48, (3, 3), activation='relu'))
+        cnn.add(layers.BatchNormalization())
         cnn.add(layers.MaxPooling2D((2, 2)))
 
         cnn.add(layers.Conv2D(48, (3, 3), activation='relu'))
+        cnn.add(layers.BatchNormalization())
         cnn.add(layers.MaxPooling2D((2, 2)))
 
         cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        cnn.add(layers.BatchNormalization())
         cnn.add(layers.MaxPooling2D((2, 2)))
 
         cnn.add(layers.GlobalAveragePooling2D())
